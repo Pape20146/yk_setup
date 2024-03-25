d9c8e68d9dafb629220c638d547bcab1616375b6
Pape <72491360+PapeSkywalker@users.noreply.github.com>
2024-03-25 11:12:21 +0100

    Refactor YK_SSH_FIDO.sh

--- scripts/YK_SSH_FIDO.sh
+++ scripts/YK_SSH_FIDO.sh
@@ -5,18 +5,23 @@ function fail()
 {
   declare -r C_ERROR="\033[1;31m"
   declare -r C_NORMAL="\033[0m"
+
   /usr/bin/echo -e "\n$(date +'%Y-%m-%dT%H:%M:%S%z') ${C_ERROR}[*EE] ${*}${C_NORMAL}\n" >&2  # Print error message with timestamp
   /usr/bin/echo -e "\e[1;33mReturning to MENU\e[0m"  # Inform user of return to menu
-  exit 2 # Exit script with error code
+
+  exit 1 # Exit script with error code
 }
 
 # Function to check the validity of the email address
-function check_email() {
+function check_email() 
+{
     local pattern="^[a-zA-Z0-9._%+-]+@domain\.com$"
     local email=""
+
     while true; do
-        read -p "Please enter an email address (must belong to the domain.com domain): " email
-        if [[ $email =~ $pattern ]]; then
+        read -p "Please enter an email address (must belong to the "domain.com" domain): " email
+        if [[ $email =~ $pattern ]]; 
+        then
             /usr/bin/echo
             /usr/bin/echo "$email"   # Returning the valid email address
             break
@@ -25,17 +30,23 @@ function check_email() {
 }
 
 # Function to handle package installation
-function pkg_installation() {
+function pkg_installation() 
+{
     /usr/bin/echo -e "\n++++++++++++++++++++ PACKAGES-INSTALLATION ++++++++++++++++++++\n"
     /usr/bin/echo -e "Packages needed: YubiKey-Manager\n"
     /usr/bin/echo "Necessary packages will be installed, if you already have them the action will be skipped"
+
     read -p "Do you want to proceed? (Y/n): " choice
     choice="${choice,,}"  # Convert to lowercase
-    if [ "$choice" == "n" ]; then
+
+    if [ "$choice" == "n" ]; 
+    then
         fail "Process terminated"
     fi
+
     # Check if the packages are already installed
-    if ! dpkg -l yubikey-manager >/dev/null 2>&1; then
+    if ! dpkg -l yubikey-manager >/dev/null 2>&1; 
+    then
         sudo apt install yubikey-manager || fail "Failed to install required packages."
         /usr/bin/echo -e "\nDone downloading packages!"
     else
@@ -44,11 +55,15 @@ function pkg_installation() {
 }
 
 # Function to erase FIDO configuration
-function erase_FIDO() {
+function erase_FIDO() 
+{
     /usr/bin/echo -e "\n++++++++++++++++++++++++ ERASING-FIDO +++++++++++++++++++++++++\n"
+
     read -p "Do you want to erase FIDO configuration? (Y/n): " choice
     choice="${choice,,}"  # Convert to lowercase
-    if [ "$choice" != "n" ]; then
+
+    if [ "$choice" != "n" ]; 
+    then
         /usr/bin/echo -e "\nErasing FIDO..."
         /usr/bin/ykman fido reset || fail "Failed to erase FIDO configuration"  # Resetting FIDO
         /usr/bin/echo -e "\nFIDO erased successfully!"
@@ -56,13 +71,17 @@ function erase_FIDO() {
 }
 
 # Function to change PIN
-function change_pin() {
+function change_pin() 
+{
     /usr/bin/echo -e "\n+++++++++++++++++++++++++ CHANGE-PIN ++++++++++++++++++++++++++\n"
     /usr/bin/echo "NOTE! Keys won't be generated if you use default PIN"
+
     read -p "Do you want to change PIN (default is 123456)? (Y/n): " choice
     /usr/bin/echo
-    choice="${choice,,}"  # Convert to lowercase     
-    if [ "$choice" != "n" ]; then
+    choice="${choice,,}"  # Convert to lowercase   
+
+    if [ "$choice" != "n" ]; 
+    then
         read -s -p "Enter current PIN: " current_pin
         /usr/bin/echo
         read -s -p "Enter new PIN: " new_pin
@@ -76,10 +95,14 @@ function change_pin() {
 function create_keys() 
 {
     /usr/bin/echo -e "\n++++++++++++++++++++++++ GENERATE-KEYS ++++++++++++++++++++++++\n"
+
     usermail=$(check_email)
+
     /usr/bin/echo -e "\nGenerating keys\n"
+
     /usr/bin/ssh-keygen -t ed25519-sk -N '' -C "$usermail" -f ~/.ssh/YK_Identity -O resident -O verify-required
     sudo chown $USER:$USER ~/.ssh/YK_Identity
+
     /usr/bin/echo "Keys done!"
 }
 
@@ -89,17 +112,23 @@ function ssh_agent_handle()
     /usr/bin/echo -e "This is your pubkey, paste it in the ~/.ssh/authorized_keys file on your ssh server\n"
     /usr/bin/cat ~/.ssh/YK_Identity.pub
     /usr/bin/echo -e "\nThe public key will be also stored in the file YK_Identity.pub created in  directory\n"
+
     read -p "When you have copied the pubkey press any enter to continue (q + enter to stop the process)" choice
     choice="${choice,,}"  # Convert to lowercase
-    if [ "$choice" == "q" ]; then
+
+    if [ "$choice" == "q" ]; 
+    then
         /usr/bin/echo -e "\nExiting script"
         exit 0
     fi
+
     /usr/bin/echo -e "\n++++++++++++++++++ ADDING-PRIVKEY-TO-AGENT ++++++++++++++++++++\n"
     /usr/bin/echo -e "Restarting agent...\n"
+
     /usr/bin/pkill ssh-agent
     eval $(ssh-agent)
     /usr/bin/ssh-add -K || fail "Unable to add private key to interface"
+
     /usr/bin/echo "Done!"
 }
 
@@ -108,7 +137,9 @@ function end_of_the_story()
 {
     /usr/bin/echo -e "\n++++++++++++++++++++++++ FINAL-ACTIONS ++++++++++++++++++++++++\n"
     /usr/bin/echo "This is your new SSH Identity:"
+
     /usr/bin/ssh-add -l || fail "Failed to show SSH Identity"
+
     /usr/bin/echo -e "\nEverything is set!\n"
     /usr/bin/echo -e "REMEMBER: every time system is rebooted or the YK is unplugged you have to add the private key to the agent with \e[1;33m/usr/bin/ssh-add -K\e[0m\n"
     /usr/bin/echo -e "When you are done with the YK SetUp script remember to restart the ssh-agent process\n"
